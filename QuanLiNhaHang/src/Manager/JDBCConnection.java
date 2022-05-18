2ad2c5c313e7adf27ceb97bb163e49318458e1b2
trumpnguyen37 <vantrung03072002@gmail.com>
2022-05-18 10:13:58 +0700

    Chinh sua giao dien va them thu vien

---
+++ QuanLiNhaHang/src/Manager/JDBCConnection.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Manager;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author FSC
+ */
+public class JDBCConnection {
+    public static Connection JDBCConnection() throws SQLServerException, SQLException{
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setUser("sa");
+        ds.setPassword("Trung@2305");
+        ds.setServerName("DESKTOP-8GKPO1M\\SQLEXPRESS");
+        ds.setPortNumber(1433);
+        ds.setDatabaseName("QuanLiNhaHang");
+        ds.setEncrypt(false);
+        ds.setTrustServerCertificate(false);
+        Connection con = null;
+        try {
+            return ds.getConnection();
+            
+        }
+            // Handle any errors that may have occurred.
+            catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+}
