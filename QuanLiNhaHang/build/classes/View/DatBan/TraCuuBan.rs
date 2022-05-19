f1b77c9fb698d6bc416a5496fca270907fd5ad09
trumpnguyen37 <vantrung03072002@gmail.com>
2022-05-19 15:15:39 +0700

    fix bug

--- QuanLiNhaHang/build/classes/View/DatBan/TraCuuBan.rs
+++
@@ -1,7 +0,0 @@
-View.TraCuuBan$6
-View.TraCuuBan$5
-View.TraCuuBan$4
-View.TraCuuBan$3
-View.TraCuuBan$2
-View.TraCuuBan$1
-View.TraCuuBan
--- QuanLiNhaHang/build/classes/View/ThongKe/ThongKeDoanhThuNam.rs
+++
@@ -1,4 +0,0 @@
-View.ThongKeDoanhThuNam$1
-View.ThongKeDoanhThuNam$2
-View.ThongKeDoanhThuNam
-View.ThongKeDoanhThuNam$3
