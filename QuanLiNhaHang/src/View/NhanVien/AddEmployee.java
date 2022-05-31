c9a7c2f85be1d63e2d9c0d5ea1dabfb8d58c500e
Viet Truong <viettruong0825@gmail.com>
2022-05-31 22:07:43 +0700

    set size set location and tat resizeable

--- QuanLiNhaHang/src/View/NhanVien/AddEmployee.java
+++ QuanLiNhaHang/src/View/NhanVien/AddEmployee.java
@@ -71,6 +71,8 @@ public class AddEmployee extends javax.swing.JFrame {
         dauBep_354 = new com.raven.suportSwing.RadioButtonCustom();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setLocation(new java.awt.Point(50, 50));
+        setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
